2539871516466a181b422912ce404aca04b6aeab
Abdallah <senior.android@procrew.pro>
2024-02-02 06:42:37 +0200

    handle storage permission

--- app/src/main/java/com/apdallahyousry/customgalleryapplication/MainActivity.kt
+++ app/src/main/java/com/apdallahyousry/customgalleryapplication/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.apdallahyousry.customgalleryapplication
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.apdallahyousry.customgalleryapplication.helpers.PermissionHelper
+import com.apdallahyousry.customgalleryapplication.helpers.PermissionHelper.PERMISSION_READ_STORAGE
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (!PermissionHelper.checkStoragePermissions(this) ) {
+            PermissionHelper.requestForStoragePermissions(this)
+        }else{
+            onStoragePermissionGranted()
+        }
+    }
+    private fun onStoragePermissionGranted(){
+
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_READ_STORAGE -> {
+                val permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+                if(permissionGranted)onStoragePermissionGranted()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/apdallahyousry/customgalleryapplication/helpers/PermissionHelper.kt
@@ -0,0 +1,40 @@
+package com.apdallahyousry.customgalleryapplication.helpers
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
+object PermissionHelper {
+
+    const val PERMISSION_READ_STORAGE = 100
+
+
+    fun checkStoragePermissions(context: Context): Boolean {
+
+
+       return  ContextCompat.checkSelfPermission(context, getReadPermission()) == PackageManager.PERMISSION_GRANTED
+    }
+    fun requestForStoragePermissions(context: Activity) {
+        ActivityCompat.requestPermissions(
+            context, arrayOf(
+               getReadPermission()
+            ),
+            PERMISSION_READ_STORAGE
+        )
+    }
+     private fun getReadPermission():String{
+         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+             //Android is 11 (R) or above
+             Manifest.permission.READ_MEDIA_IMAGES
+         } else {
+             //Below android 11
+             Manifest.permission.READ_EXTERNAL_STORAGE
+         }
+
+     }
+}
