465c05cf7f07777ef70bcb06a0a6f904ddd6ec66
Abdallah <senior.android@procrew.pro>
2024-02-02 21:56:38 +0200

    add ui test for toggle recyclerview  grid and linear

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,9 @@ dependencies {
     testImplementation ("androidx.arch.core:core-testing:2.1.0")
     testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.4.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-contrib:3.4.0")
+
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test:rules:1.5.0")
 }
